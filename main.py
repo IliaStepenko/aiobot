f165ed362c20c3222e16cfcfdfcec5eb18e8fa42
IliaStepenko <stepenkoila@gmail.com>
2020-11-02 15:59:00 +0200

    1
    1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher.filters import Command
 from aiogram.types import InputFile, ContentTypes
 from aiogram.utils.executor import start_webhook
-from cv2 import cv2
+
 
 TOKEN = '1019187234:AAFDX_Omr5ipqZUxjUc7e-HwwqLgENe3Y9E'
 
@@ -37,35 +37,6 @@ async def welcome(message: types.Message):
 @dp.message_handler()
 async def echo(message: types.Message):
     await bot.send_message(message.chat.id, message.text)
-
-async def create_gif_from_image(filename):
-    frame = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
-
-    res_file_name = f"{filename.replace('.jpg','')}.mp4"
-    print(res_file_name)
-    frame_rate = [frame for i in range(1, 4)]
-    height, width, layers = frame.shape
-    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-    video = cv2.VideoWriter(res_file_name, fourcc, 1,  (width, height))
-
-    for fr in frame_rate:
-        video.write(fr)
-
-    video.release()
-
-    cv2.destroyAllWindows()
-    return res_file_name
-
-
-@dp.message_handler(Command(['gif', 'Gif'], ignore_caption=False), content_types=ContentTypes.PHOTO)
-async def send_gif(message: types.message):
-    from_id = message['chat']['id']
-    file_name = f'pp1_{from_id}.jpg'
-    await message.photo[-1].download(file_name)
-    video_file_name = await create_gif_from_image(file_name)
-    video = InputFile(video_file_name)
-    await bot.send_animation(from_id, video, duration=3)
-
 
 
 async def on_startup(dp):
