3368d95427c7055c6268a90cd267b8fea46ab25f
IliaStepenko <stepenkoila@gmail.com>
2020-11-02 12:57:52 +0200

    delete img to gif function =)

--- main.py
+++ main.py
@@ -32,36 +32,6 @@ async def welcome(message: types.Message):
         disable_web_page_preview=True)
 
 
-async def create_gif_from_image(filename):
-    frame = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
-
-    res_file_name = f"{filename.replace('.jpg','')}.mp4"
-    print(res_file_name)
-    frame_rate = [frame for i in range(1, 4)]
-    height, width, layers = frame.shape
-    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-    video = cv2.VideoWriter(res_file_name, fourcc, 1,  (width, height))
-
-    for fr in frame_rate:
-        video.write(fr)
-
-    video.release()
-
-    cv2.destroyAllWindows()
-    return res_file_name
-
-
-@dp.message_handler(Command(['gif', 'Gif'], ignore_caption=False), content_types=ContentTypes.PHOTO)
-async def echo(message: types.message):
-    from_id = message['chat']['id']
-    file_name = f'pp1_{from_id}.jpg'
-    print(file_name)
-    await message.photo[-1].download(file_name)
-    video_file_name = await create_gif_from_image(file_name)
-    video = InputFile(video_file_name)
-    await bot.send_animation(from_id, video, duration=3)
-
-
 
 
 @dp.message_handler()
